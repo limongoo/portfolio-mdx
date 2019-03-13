97adf3b604994113c2b7d6b50d462da17ebb417a
Ivan Limongan <ivan.limongan@gmail.com>
2019-03-13 15:00:54 -0700

    project list component

--- src/components/project-list.js
+++ src/components/project-list.js
@@ -1,5 +1,42 @@
 import React from "react"
-import { css } from "@emotion/core"
-import { rhythm } from "../utils/typography"
+// import { css } from "@emotion/core"
+// import { rhythm } from "../utils/typography"
 import { StaticQuery, Link, graphql } from "gatsby"
 
+export default () => (
+    <StaticQuery
+        query={graphql`
+            query PageList{
+                allMdx(sort: { fields: [frontmatter___date], order: DESC }){
+                    edges{
+                        node{
+                            fields {
+                                route
+                            }
+                            frontmatter{
+                                title
+                                date
+                            }
+                            timeToRead
+                            excerpt
+                        }
+                    }
+                }
+            }
+        `
+        }  
+        
+        render={data => (
+            data.allMdx.edges.map(({ node }, index) => (
+                <Link to={node.fields.route} style={{textDecoration:'none', color:  'inherit'}} key={index}>
+                    <div className="post-item">
+                        <h3 className="post-title">{node.frontmatter.title}</h3>
+                        <p className="post-excerpt">{node.excerpt}</p>
+                        <p className="post-date">{node.frontmatter.date}</p>
+                    </div>
+                </Link>
+            ))
+        )}
+
+    />
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,72 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+// import { Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
 import Layout from '../components/layout'
-// import Projectlist from "../components/project-list"
+import Projectlist from "../components/project-list"
 import './index.css'
 
-export default ({data}) => {
-    const posts = data.allMdx.edges
-    return (
-        <Layout>
-            <div>
-                <header>
-                    <h2>Portfolio Board</h2>
-                </header>
-                <div>
-                    {posts.map(({node}, index)=> (
-                        <Link to={node.fields.route} style={{textDecoration:'none', color:  'inherit'}} key={index}>
-                            <div className="post-item">
-                                <h3 className="post-title">{node.frontmatter.title}</h3>
-                                <p className="post-excerpt">{node.excerpt}</p>
-                                <p className="post-date">{node.frontmatter.date}</p>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </Layout>
-    )
-}
 
-export const query = graphql`query PageList{
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }){
-        edges{
-            node{
-                fields {
-                    route
-                }
-                frontmatter{
-                    title
-                    date
-                }
-                timeToRead
-                excerpt
-            }
-        }
-    }
-}`
+export default ({ data }) => {
+    console.log(data)
+    return (
+      <Layout>
+        <div>
+          <h3
+            css={css`
+              display: inline-block;
+              border-bottom: 1px solid;
+            `}
+          >
+            My Portfolio
+          </h3>
+  
+          <Projectlist></Projectlist>
+        </div>
+      </Layout>
+    )
+  }
+
+
+
+
+// export default ({data}) => {
+//     const posts = data.allMdx.edges
+//     return (
+//         <Layout>
+//             <div>
+//                 <header>
+//                     <h2>Portfolio Board</h2>
+//                 </header>
+//                 <div>
+//                     {posts.map(({node}, index)=> (
+//                         <Link to={node.fields.route} style={{textDecoration:'none', color:  'inherit'}} key={index}>
+//                             <div className="post-item">
+//                                 <h3 className="post-title">{node.frontmatter.title}</h3>
+//                                 <p className="post-excerpt">{node.excerpt}</p>
+//                                 <p className="post-date">{node.frontmatter.date}</p>
+//                             </div>
+//                         </Link>
+//                     ))}
+//                 </div>
+//             </div>
+//         </Layout>
+//     )
+// }
+
+// export const query = graphql`query PageList{
+//     allMdx(sort: { fields: [frontmatter___date], order: DESC }){
+//         edges{
+//             node{
+//                 fields {
+//                     route
+//                 }
+//                 frontmatter{
+//                     title
+//                     date
+//                 }
+//                 timeToRead
+//                 excerpt
+//             }
+//         }
+//     }
+// }`
